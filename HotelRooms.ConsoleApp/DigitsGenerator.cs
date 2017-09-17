f59daa8abe0dde22fc78ca9993ff82b4563b00f2
J <jawahirlal@gmail.com>
2017-09-17 16:17:40 +0100

    Initial Commit

---
+++ HotelRooms.ConsoleApp/DigitsGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRooms.ConsoleApp
+{
+    public class DigitsGenerator
+    {
+
+        int[] digits;
+
+        public int[] GenerateDigitsCount(int numberRoomsReq)
+        {
+            digits = new int[10];
+
+            //Generator divisor to the length of the number of rooms
+            // i.e. if 55 then 10 
+            // if 345 then 100
+            int divisor =(int) Math.Pow(10, Math.Floor(Math.Log10(numberRoomsReq)));
+
+            CountDigits(numberRoomsReq, numberRoomsReq, divisor);
+
+            return digits;
+        }
+
+        private void CountDigits(int roomsReq, int numberRooms, int divisor)
+        {
+            if (numberRooms < 10) //for less than 10 rooms
+            {
+                IncreaseDigitCount(1, numberRooms, 1);
+                numberRooms = 0;
+            }
+            else
+            {
+                int divisorMultiple = numberRooms / divisor;
+                int divisorRemainder = numberRooms % divisor;
+
+
+                if (divisorMultiple > 1) //if the out is multiple of divisor i.e. 555 then add 100 to 1-4
+                {
+                    IncreaseDigitCount(1, divisorMultiple - 1, divisor);
+                }
+
+                //For each multiple of 10 add result 
+                //i.e. 215
+                // then result is 2
+                // divistor is 100 and Log is 2
+                // multiplied by divisor/10 
+                // 2 * 2 * 10 = 40 for 200
+                IncreaseDigitCount(0, 9, divisorMultiple * (int)Math.Log10(divisor) * (divisor / 10));
+
+
+                //because 1-9 does not contain zeros if the
+                //number is greater than 100 i.e. 106 
+                // then in the line above we have added 10 additional zoros
+                // 101 - 106 should contains 6 zeros and delete additional 4 zeros
+                if (roomsReq > 99 && divisorRemainder < 10 && divisor > 10)
+                    IncreaseDigitCount(0, 0, -(9 - divisorRemainder));
+
+
+                IncreaseDigitCount(divisorMultiple, divisorMultiple, divisorRemainder + 1);
+                numberRooms -= (divisor * divisorMultiple);
+
+                if(numberRooms > 0)
+                    CountDigits(roomsReq, numberRooms, divisor /10);
+            }
+        }
+
+
+        private void IncreaseDigitCount(int fromIndex, int toIndex, int increaseBy)
+        {
+            for (int i = fromIndex; i <= toIndex; i++)
+                digits[i] += increaseBy;
+        }
+
+    }
+}
---
+++ HotelRooms.Tests/DigitsGeneratorTests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using HotelRooms.ConsoleApp;
+
+namespace HotelRooms.Tests
+{
+    [TestClass]
+    public class HotelRoomsTests
+    {
+        DigitsGenerator digitsGenerator;
+        public HotelRoomsTests()
+        {
+            digitsGenerator = new DigitsGenerator();
+        }
+ 
+        [TestMethod]
+        public void Test_When_Passed_1_Should_Return_1()
+        {
+            //arrange
+            var numberRooms = 1;
+            var exptectedValues  = new  int [10] { 0,1,0,0,0,0,0,0,0,0 };
+
+            checkValues(numberRooms,  exptectedValues);
+        }
+
+
+        [TestMethod]
+        public void Test_When_Passed_2_Should_Return()
+        {
+            //arrange
+            var numberRooms = 2;
+            var exptectedValues = new int[10] { 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+        [TestMethod]
+        public void Test_When_Passed_9_Should_Return()
+        {
+            //arrange
+            var numberRooms = 9;
+            var exptectedValues = new int[10] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+        [TestMethod]
+        public void Test_When_Passed_15_Should_Return()
+        {
+            //arrange
+            var numberRooms = 15;
+            var exptectedValues = new int[10] { 1, 8, 2, 2, 2, 2, 1, 1, 1, 1 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+
+        [TestMethod]
+        public void Test_When_Passed_56_Should_Return()
+        {
+            //arrange
+            var numberRooms = 56;
+            var exptectedValues = new int[10] { 5, 16, 16, 16, 16, 13, 6, 5, 5, 5 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+
+        [TestMethod]
+        public void Test_When_Passed_99_Should_Return()
+        {
+            //arrange
+            var numberRooms = 99;
+            var exptectedValues = new int[10] { 9, 20, 20, 20, 20, 20, 20, 20, 20, 20 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+        [TestMethod]
+        public void Test_When_Passed_100_Should_Return()
+        {
+            //arrange
+            var numberRooms = 100;
+            var exptectedValues = new int[10] { 11, 21, 20, 20, 20, 20, 20, 20, 20, 20 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+
+        [TestMethod]
+        public void Test_When_Passed_101_Should_Return()
+        {
+            //arrange
+            var numberRooms = 101;
+            var exptectedValues = new int[10] { 12, 23, 20, 20, 20, 20, 20, 20, 20, 20 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+
+        [TestMethod]
+        public void Test_When_Passed_115_Should_Return()
+        {
+            //arrange
+            var numberRooms = 115;
+            var exptectedValues = new int[10] { 21, 44, 22, 22, 22, 22, 21, 21, 21, 21 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+
+        [TestMethod]
+        public void Test_When_Passed_120_Should_Return()
+        {
+            //arrange
+            var numberRooms = 120;
+            var exptectedValues = new int[10] { 22, 53, 23, 22, 22, 22, 22, 22, 22, 22 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+
+        [TestMethod]
+        public void Test_When_Passed_200_Should_Return()
+        {
+            //arrange
+            var numberRooms = 200;
+            var exptectedValues = new int[10] { 31, 140, 41, 40, 40, 40, 40, 40, 40, 40 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+        [TestMethod]
+        public void Test_When_Passed_205_Should_Return()
+        {
+            //arrange
+            var numberRooms = 205;
+            var exptectedValues = new int[10] { 36, 141, 47, 41, 41, 41, 40, 40, 40, 40 };
+
+            checkValues(numberRooms, exptectedValues);
+        }
+
+
+
+        private void checkValues(int numberRooms, int[] exptectedValues)
+        {
+            //act
+            var digits = digitsGenerator.GenerateDigitsCount(numberRooms);
+
+
+            //assert
+            Assert.IsTrue(digits.SequenceEqual(exptectedValues));
+        }
+    }
+}
+
